4306ad370853bfac48e000fdf42c16508bb97f21
startergate <startergate36@gmail.com>
2020-05-15 12:52:55 +0900

    0515

--- LearnSmartCultureAppContent/app/src/main/java/com/example/learn_smartcultureappcontent/SignUpActivity.java
+++ LearnSmartCultureAppContent/app/src/main/java/com/example/learn_smartcultureappcontent/SignUpActivity.java
@@ -1,13 +1,27 @@
 package com.example.learn_smartcultureappcontent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -27,11 +41,7 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (checkVaild()) {
-                    Intent intent = new Intent();
-                    intent.putExtra("id", id_et.getText().toString());
-                    intent.putExtra("pw", pw_et.getText().toString());
-                    setResult(RESULT_OK, intent);
-                    finish();
+                    new HttpAsyncTask().execute();
                 }
             }
         });
@@ -47,5 +57,48 @@ public class SignUpActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+
+            HttpUrl httpUrl = new HttpUrl.Builder()
+                        .scheme("http").host("13.209.236.238")
+                        .addPathSegment("user_join.php")
+                        .addQueryParameter("userid", id_et.getText().toString())
+                        .addQueryParameter("passwd", pw_et.getText().toString())
+                        .build();
+
+            try {
+                Request req = new Request.Builder().url(httpUrl).build();
+                Response res = client.newCall(req).execute();
+                result = res.body().string();
+                // Log.d(tag, "doInBackground: " + res.body().string());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if (s.contains("failed")) {
+                Log.d("SignupActivity", "failed");
+            } else {
+                Intent intent = new Intent();
+                intent.putExtra("id", id_et.getText().toString());
+                intent.putExtra("pw", pw_et.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
     }
 }
