3b71e6f471ecf578fa5cb6e61af33b792b2ff19f
startergate <startergate36@gmail.com>
2020-05-22 16:02:41 +0900

    0522

---
+++ ViewPagerSample/app/src/main/java/com/example/viewpagersample/MainFragment.java
@@ -0,0 +1,38 @@
+package com.example.viewpagersample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import devlight.io.library.ntb.NavigationTabBar;
+
+public class MainFragment extends Fragment implements View.OnClickListener, ViewPager.OnPageChangeListener {
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
